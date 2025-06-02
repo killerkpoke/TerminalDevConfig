cf33b59d40a2a52a6654f6de1b37b2e123777cd7
Kpoke <34098206+killerkpoke@users.noreply.github.com>
2025-06-02 18:10:01 +0200

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,12 +3,12 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 
-config.font = wezterm.font 'Monocraft'
---config.font = wezterm.font 'Miracode'
+--config.font = wezterm.font 'Monocraft'
+config.font = wezterm.font 'Miracode'
 
-config.font_size = 17
+config.font_size = 18
 
 config.color_scheme = 'Gruvbox Dark (Gogh)'
 
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.9
 return config
