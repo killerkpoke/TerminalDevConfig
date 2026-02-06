d44d240ad4d96185b97e893cdeb1a8e62d682964
Kpoke <34098206+killerkpoke@users.noreply.github.com>
2026-02-06 16:18:10 +0100

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,13 +2,13 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-
---config.font = wezterm.font 'Monocraft'
 config.font = wezterm.font 'Miracode'
 
 config.font_size = 18
 
-config.color_scheme = 'Gruvbox Dark (Gogh)'
+--config.color_scheme = 'Ibm 3270 (High Contrast) (Gogh)'
+local theme = require 'colors.0x96f'
+config.colors = theme
 
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.7
 return config
